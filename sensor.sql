e004551bd299b4258ff4ddabc55b6ab9d367b927
Niclas Heide <nikiheide@gmail.com>
2025-02-28 09:28:57 +0100

    Added SQL statement for the database

---
+++ sensor.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS sensor (
+    sensor_id INTEGER PRIMARY KEY,
+    type_name TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS location (
+    location_id INTEGER PRIMARY KEY,
+    lat REAL,
+    lon REAL
+);
+
+ CREATE TABLE IF NOT EXISTS sds_data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sensor_id INTEGER,
+    location_id INTEGER,
+    timestamp TEXT,
+    p1 REAL,
+    dur_p1 REAL,
+    ratio_p1 REAL,
+    p2 REAL,
+    dur_p2 REAL,
+    ratio_p2 REAL,
+    FOREIGN KEY (sensor_id) REFERENCES sensor_type(sensor_id),
+    FOREIGN KEY (location_id) REFERENCES location(location_id)
+);
+
+CREATE TABLE IF NOT EXISTS dht_data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sensor_id INTEGER,
+    location_id INTEGER,
+    timestamp TEXT,
+    temperature REAL,
+    humidity REAL,
+    FOREIGN KEY (sensor_id) REFERENCES sensor_type(sensor_id),
+    FOREIGN KEY (location_id) REFERENCES location(location_id)
+);
