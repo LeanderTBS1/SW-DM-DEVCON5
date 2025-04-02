0947301c2407ad1a586073bca827299764ba9f87
Niclas Heide <niclas.heide@students.tbs1.de>
2025-04-02 10:12:34 +0200

    Added QueriesHandler class and example usage

---
+++ queries.py
@@ -0,0 +1,29 @@
+import sqlite3, csv ,os
+from pathlib import Path
+
+class QueriesHandler:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        db_dir = Path(self.db_path).parent
+        if not db_dir.exists():
+            db_dir.mkdir(parents=True)
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+
+    def get_avg(self):
+        data = self.cursor.execute("""
+            SELECT 
+                AVG(temperature) AS avg_temperature
+            FROM dht_data
+            WHERE DATE(timestamp) = '2022-03-14';
+        """)
+        return data.fetchone()
+
+if __name__ == "__main__":
+    path = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(path, "data.db")
+
+    db = QueriesHandler(db_path)
+    print(db.get_avg())
+    """MAX(temperature) AS max_temperature,
+            MIN(temperature) AS min_temperature,"""
